dcb4c012b69e274dc46a34400ebd21b9e4cf48f3
Madan Gopal <99560147+madangopal962@users.noreply.github.com>
2026-03-01 22:58:12 +0530

    Add files via upload

--- index.php
+++ index.php
@@ -1,3 +1,6 @@
+<?php 
+include('connection.php');
+?>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -12,15 +15,23 @@
 
 <div class="container mt-3">
 <div class="row">
+<?php 
+$sql="select * from courses";
+$result=mysqli_query($conn,$sql);
+
+while($data=mysqli_fetch_array($result))
+{?>
 <div class="col-xl-4">
   <div class="card" style="width:400px">
+    <img class="card-img-top" src="uploads/<?php echo $data['image'];?>" alt="Card image" style="width:100%">
     <div class="card-body">
-      <h4 class="card-title">Java</h4>
-      <p class="card-text">Rs 500</p>
-      <a href="javascript:void(0)" class="btn btn-primary buynow">Buy Now</a>
+      <h4 class="card-title"><?php echo $data['name'];?></h4>
+      <p class="card-text">Rs <?php echo $data['price'];?></p>
+      <a href="javascript:void(0)" data-productid="<?php echo $data['id'];?>" data-productname="<?php echo $data['name'];?>" data-amount="<?php echo $data['price'];?>" class="btn btn-primary buynow">Buy Now</a>
     </div>
   </div>
 </div>
+<?php } ?>
 
 </div>
 </div>
@@ -31,13 +42,42 @@
 
 $(".buynow").click(function()
 {
+
+var amount=$(this).attr('data-amount');	
+var productid=$(this).attr('data-productid');	
+var productname=$(this).attr('data-productname');	
+	
 var options = {
-    "key": "rzp_test_SKFrvWEajlFaxL", // Enter the Key ID generated from the Dashboard
-    "amount": "5000", // Amount is in currency subunits. Default currency is INR. Hence, 50000 refers to 50000 paise
-    "name": "Lotus365",
-    "description": "none",
+    "key": "rzp_test_zHhNFsppG7bIjH", // Enter the Key ID generated from the Dashboard
+    "amount": amount*100, // Amount is in currency subunits. Default currency is INR. Hence, 50000 refers to 50000 paise
+    "name": "The Digital Oceans",
+    "description": productname,
     "image": "https://example.com/your_logo",
-    "theme": "color": "#3399cc"
+    "handler": function (response){
+        var paymentid=response.razorpay_payment_id;
+		
+		$.ajax({
+			url:"payment-process.php",
+			type:"POST",
+			data:{product_id:productid,payment_id:paymentid},
+			success:function(finalresponse)
+			{
+				if(finalresponse=='done')
+				{
+					window.location.href="http://localhost/php-practical-work/payment-gateway/razorpay/success.php";
+				}
+				else 
+				{
+					alert('Please check console.log to find error');
+					console.log(finalresponse);
+				}
+			}
+		})
+        
+    },
+    "theme": {
+        "color": "#3399cc"
+    }
 };
 var rzp1 = new Razorpay(options);
  rzp1.open();
@@ -46,6 +86,3 @@ var rzp1 = new Razorpay(options);
 </script>
 </body>
 </html>
-
-
-
